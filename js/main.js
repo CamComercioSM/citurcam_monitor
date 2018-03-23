c2b3f2f573cae15815d0f7b19284159c323ab444
jpllinasCCSM <jpllinas@ccsm.org.co>
2018-03-23 08:56:27 -0500

    ajustes hechos sobre el pc monitor.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 
 var audioElementBeep = document.createElement('audio');
 audioElementBeep.setAttribute('src', '/snd/Jetsons-doorbell.mp3');
-audioElementBeep.volumen = 0.5;
+//audioElementBeep.volumen = 0.5;
 function beep() {
     // if(audioElementBeep.ended){
-        audioElementBeep.play();
+    audioElementBeep.play();
     // }
 }
-
-
 
 
 var SEDE = 1;
@@ -22,81 +20,81 @@ var ModulosActivos = new Array();
 var TurnosLlamando = new Array();
 var tiempoConsultarTurnoLlamando;
 
-function iniciarMonitorDeTurnos(anchoModulo, altoModulo, tiempoConsulta, tiempoPresentacion ){
+function iniciarMonitorDeTurnos(anchoModulo, altoModulo, tiempoConsulta, tiempoPresentacion) {
     tiempoMostrarTurnoLlamando = tiempoPresentacion;
     tiempoConsultarTurnoLlamando = tiempoConsulta
-    mostrarModulosAtencionActivos( anchoModulo, altoModulo );
+    mostrarModulosAtencionActivos(anchoModulo, altoModulo);
     mostrarDatosTurnoLlamando();
 }
-function mostrarModulosAtencionActivos( anchoModulo, altoModulo){
+function mostrarModulosAtencionActivos(anchoModulo, altoModulo) {
     var jqxhr = $.ajax({
         method: "POST",
         url: "api.php",
-        dataType : "html",
-        data: { operacion: "modulosActivos", sedeID: SEDE, areaID:AREA_TRABAJO  }
-    }).done(function(data) {
-    // console.log( "Modulos de Atencion" );
+        dataType: "html",
+        data: {operacion: "modulosActivos", sedeID: SEDE, areaID: AREA_TRABAJO}
+    }).done(function (data) {
+        // console.log( "Modulos de Atencion" );
         // console.log( data );
         data = JSON.parse(data);
-        if(data.RESPUESTA === 'EXITO'){
+        if (data.RESPUESTA === 'EXITO') {
             arrancarCarruselTurnos(anchoModulo, altoModulo, data);
-         	setInterval( buscarTurnosLLamando, tiempoConsultarTurnoLlamando );
-        // 			buscarTurnosAtendiendo();
+            setInterval(buscarTurnosLLamando, tiempoConsultarTurnoLlamando);
+            // 			buscarTurnosAtendiendo();
         }
     })
-    .fail(function(data) {
-        console.log( "Falló la consulta. Contactar con el Centro TICS." );
-        console.log( data );
-        // setTimeout( function(){ location.reload() }, 3000);
-    });
-      
+            .fail(function (data) {
+                console.log("Falló la consulta. Contactar con el Centro TICS.");
+                console.log(data);
+                // setTimeout( function(){ location.reload() }, 3000);
+            });
+
 }
 
-function arrancarCarruselTurnos(anchoModulo, altoModulo, data){
+function arrancarCarruselTurnos(anchoModulo, altoModulo, data) {
     var Things = data.DATOS;
-    for (var i = 0; i  < Things.length; i++) {
+    for (var i = 0; i < Things.length; i++) {
         var Modulo = [];
         Modulo["id"] = Things[i].moduloAtencionID;
         Modulo["codigo"] = Things[i].moduloAtencionCODIGO;
         Modulo["nombre"] = Things[i].moduloAtencionTITULO;
-        ModulosActivos.push( Modulo ); 
+        ModulosActivos.push(Modulo);
         $("#carousel").append(
-            '<div id="modulo-id-' + Modulo["id"] + '" class="table turno-atendiendo">'
-            +'<div id="modulo-turno-' + Modulo["id"] + '" class="col-xs-8 nombre-turno " >LIBRE</div>'
-            +'<div id="modulo-codigo-' + Modulo["id"] + '" class="col-xs-4 modulo-turno " style="background-image: url(/img/fondo-turno-llamando.png);" >' + Modulo["nombre"] + '</div>'
-            +'</div>'
-        );
+                '<div id="modulo-id-' + Modulo["id"] + '" class="table turno-atendiendo">'
+                + '<div id="modulo-turno-' + Modulo["id"] + '" class="col-xs-8 nombre-turno " >LIBRE</div>'
+                + '<div id="modulo-codigo-' + Modulo["id"] + '" class="col-xs-4 modulo-turno " style="background-image: url(/img/fondo-turno-llamando.png);" >' + Modulo["nombre"] + '</div>'
+                + '</div>'
+                );
     }
     var pasos = 4;
-    if( ModulosActivos.length < pasos ){
+    if (ModulosActivos.length < pasos) {
         pasos = ModulosActivos.length;
     }
-    
-	carrusel = $( "#carousel" ).rcarousel({
-      width: anchoModulo, height: altoModulo,
-      step: 4, visible: pasos,
-	  speed: 5000,
-      auto: {
-      	enabled: true,
-      	interval: 1357,
-      	direction: "next"
-      }
-	});
+
+    carrusel = $("#carousel").rcarousel({
+        width: anchoModulo, height: altoModulo,
+        step: 4, visible: pasos,
+        speed: 5000,
+        auto: {
+            enabled: true,
+            interval: 1357,
+            direction: "next"
+        }
+    });
 }
-function buscarTurnosLLamando(){
-     var jqxhr = $.ajax({
+function buscarTurnosLLamando() {
+    var jqxhr = $.ajax({
         method: "POST",
         url: "api.php",
-        dataType : "html",
-        data: { operacion: "turnosLlamando", sedeID: SEDE, areaID:AREA_TRABAJO }
-      }).done(function(data) {
-        console.log( "Siguiente" );
+        dataType: "html",
+        data: {operacion: "turnosLlamando", sedeID: SEDE, areaID: AREA_TRABAJO}
+    }).done(function (data) {
+        console.log("Siguiente");
         // console.log( data );
         data = JSON.parse(data);
         TurnosLlamando = new Array();
-        if(data.RESPUESTA === 'EXITO'){
+        if (data.RESPUESTA === 'EXITO') {
             var TurnosRecibidos = data.DATOS;
-            if(TurnosRecibidos.length){
+            if (TurnosRecibidos.length) {
                 for (var i in TurnosRecibidos) {
                     var Turno = [];
                     Turno["id"] = TurnosRecibidos[i].turnoID;
@@ -106,58 +104,59 @@ function buscarTurnosLLamando(){
                     Turno["identificacion"] = TurnosRecibidos[i].Persona.personaIDENTIFICACION;
                     Turno["moduloID"] = TurnosRecibidos[i].ModuloAtencion.moduloAtencionID;
                     Turno["modulo"] = TurnosRecibidos[i].ModuloAtencion.moduloAtencionTITULO;
-                    TurnosLlamando.push( Turno ); 
+                    TurnosLlamando.push(Turno);
                 }
+                mostrarDatosTurnoLlamando();
             }
         }
-        console.log( "EN LISTA" );
-        console.log( TurnosLlamando );
-        setTimeout(buscarTurnosLLamando, tiempoConsultarTurnoLlamando );
-      }).fail(function(data) {
-        console.clear();  
-        console.log( "Falló la consulta. Contactar con el Centro TICS." );
-        console.log( data );
+        console.log("EN LISTA");
+        console.log(TurnosLlamando);
+        setTimeout(buscarTurnosLLamando, tiempoConsultarTurnoLlamando);
+    }).fail(function (data) {
+        console.clear();
+        console.log("Falló la consulta. Contactar con el Centro TICS.");
+        console.log(data);
         // setTimeout( function(){ location.reload() }, 3000);
-      });
-    
+    });
 }
-function mostrarDatosTurnoLlamando(){
-    
-    console.log( "mostra en panel" );
-    if(TurnosLlamando.length > 0){
-        console.log( " SI " );
+function mostrarDatosTurnoLlamando() {
+
+    console.log("mostra en panel");
+    if (TurnosLlamando.length > 0) {
+        console.log(" SI ");
         beep();
-        if( TurnosLlamando[mostrando].nombre && TurnosLlamando[mostrando].apellido ){
-            $("#nombre-turno-llamando").html(TurnosLlamando[mostrando].nombre+ " "+ TurnosLlamando[mostrando].apellido);
+        if (TurnosLlamando[mostrando].nombre && TurnosLlamando[mostrando].apellido) {
+            $("#nombre-turno-llamando").html(TurnosLlamando[mostrando].nombre + " " + TurnosLlamando[mostrando].apellido);
         }
         $("#codigo-modulo-llamando").html(TurnosLlamando[mostrando].modulo);
-        
-         cargarDatosTurnoLlamandoAlCarrusel(
-            TurnosLlamando[mostrando].moduloID,
-            TurnosLlamando[mostrando].modulo,
-             TurnosLlamando[mostrando].nombre,
-             TurnosLlamando[mostrando].apellido
-        );
+
+        cargarDatosTurnoLlamandoAlCarrusel(
+                TurnosLlamando[mostrando].moduloID,
+                TurnosLlamando[mostrando].modulo,
+                TurnosLlamando[mostrando].nombre,
+                TurnosLlamando[mostrando].apellido
+                );
         mostrando++;
-        if(mostrando >= TurnosLlamando.length) mostrando = 0;
-    }else{
-        console.log( " NO " );
+        if (mostrando >= TurnosLlamando.length)
+            mostrando = 0;
+    } else {
+        console.log(" NO ");
         mostrando = 0;
         $("#nombre-turno-llamando").html("");
         $("#codigo-modulo-llamando").html("");
     }
-    
-    setTimeout(mostrarDatosTurnoLlamando, tiempoMostrarTurnoLlamando );
+
+    setTimeout(mostrarDatosTurnoLlamando, tiempoMostrarTurnoLlamando);
 }
 function cargarDatosTurnoLlamandoAlCarrusel(moduloID, moduloCODIGO, turnoNOMBRE, turnoAPELLIDO) {
-    var nombreCompleto = turnoNOMBRE+ " "+turnoAPELLIDO;
-    var data = carrusel.data( "data" );
-    for( var i in data.paths ){
+    var nombreCompleto = turnoNOMBRE + " " + turnoAPELLIDO;
+    var data = carrusel.data("data");
+    for (var i in data.paths) {
         var elemCarrusel = data.paths[i][0];
-        if( elemCarrusel.id == 'modulo-id-' + moduloID + '' ){
-            $("#carousel #modulo-turno-"+moduloID).html( nombreCompleto );
-            $div = $( elemCarrusel );
-            $div.find('#modulo-turno-' + moduloID).html( nombreCompleto  );
+        if (elemCarrusel.id == 'modulo-id-' + moduloID + '') {
+            $("#carousel #modulo-turno-" + moduloID).html(nombreCompleto);
+            $div = $(elemCarrusel);
+            $div.find('#modulo-turno-' + moduloID).html(nombreCompleto);
         }
     }
 }
@@ -178,7 +177,7 @@ function cargarDatosTurnoLlamandoAlCarrusel(moduloID, moduloCODIGO, turnoNOMBRE,
 //             var TurnosRecibidos = data.DATOS;
 //             if(TurnosRecibidos.length){
 //                 for (var i in TurnosRecibidos) {
-                    
+
 //                     var Turno = [];
 //                     Turno["id"] = TurnosRecibidos[i].turnoID;
 //                     Turno["codigo"] = TurnosRecibidos[i].turnoCODIGO;
@@ -187,10 +186,10 @@ function cargarDatosTurnoLlamandoAlCarrusel(moduloID, moduloCODIGO, turnoNOMBRE,
 //                     Turno["identificacion"] = TurnosRecibidos[i].Persona.personaIDENTIFICACION;
 //                     Turno["moduloID"] = TurnosRecibidos[i].ModuloAtencion.moduloAtencionID;
 //                     Turno["modulo"] = TurnosRecibidos[i].ModuloAtencion.moduloAtencionTITULO;
-                    
+
 //                     TurnosLlamando.push( Turno ); 
-                    
-                    
+
+
 //                     // console.log(TurnosRecibidos[i].Persona.personaNOMBRES + " " + TurnosRecibidos[i].Persona.personaAPELLIDOS);
 //                     // console.log(TurnosRecibidos[i].Persona.personaIDENTIFICACION);
 //                     // console.log(TurnosRecibidos[i].ModuloAtencion.moduloAtencionCODIGO);
@@ -199,10 +198,10 @@ function cargarDatosTurnoLlamandoAlCarrusel(moduloID, moduloCODIGO, turnoNOMBRE,
 //                 }
 //             }
 //         }
-        
+
 //         clearTimeout(tiempoTurnoLlamando);
 //         tiempoTurnoLlamando = setTimeout(mostrarTurnoLlamando, tiempo );
-        
+
 //       }).fail(function(data) {
 //         console.clear();  
 //         console.log( "Falló la consulta. Contactar con el Centro TICS." );
@@ -269,14 +268,14 @@ function cargarDatosTurnoLlamandoAlCarrusel(moduloID, moduloCODIGO, turnoNOMBRE,
 //         console.log( data );
 //         setTimeout( function(){ location.reload() }, 3000);
 //       });
-      
+
 // }
 
 
 
 // function mostrarTurnoLlamando(tiempo){
 
-      
+
 // }
 
 
--- libs/ApiSicam.clase.php
+++ libs/ApiSicam.clase.php
@@ -2,79 +2,81 @@
 
 class ApiSicam {
 
-//   const URL = 'https://api.ccsm.org.co/';
-    const URL = 'https://sicam32-jpllinas.c9users.io/api/';
+    const URL = 'https://api.ccsm.org.co/';
+//    const URL = 'https://sicam32-jpllinas.c9users.io/api/';
     const USERNAME = 'ccsm_monitor_turnos';
     const PASSWORD = 'kpaC6DdtRBWj82k';
+
 //   const USERNAME = 'afiliados@ccsm.org.co';
 //   const PASSWORD = 'afiliados@ccsm.org.co';
-   
-   
-   private $conexionApi = null;
+
+
+    private $conexionApi = null;
 
     /**
      *
      * @param <array> $postFields
      * @return SimpleXMLElement
      * @desc this connects but also sends and retrieves 
-           the information returned in XML
+      the information returned in XML
      */
-    public function probarConexion($postFields = null){
+    public function probarConexion($postFields = null) {
         $estadoConexion = false;
         $this->conexionApi = curl_init();
-        curl_setopt($this->conexionApi, CURLOPT_URL, self::URL."conectar");
+        curl_setopt($this->conexionApi, CURLOPT_URL, self::URL . "conectar");
         curl_setopt($this->conexionApi, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($this->conexionApi, CURLOPT_USERPWD, self::USERNAME.":".self::PASSWORD);
+        curl_setopt($this->conexionApi, CURLOPT_USERPWD, self::USERNAME . ":" . self::PASSWORD);
         curl_setopt($this->conexionApi, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
         $output = curl_exec($this->conexionApi);
         print_r($output);
         $respuesta = json_decode($output);
         if (json_last_error() === JSON_ERROR_NONE) {
-            if($respuesta->RESPUESTA == 'EXITO'){
+            if ($respuesta->RESPUESTA == 'EXITO') {
                 session_start();
                 $estadoConexion = $_SESSION['API_CONEXION'] = true;
                 session_write_close();
                 $info = curl_getinfo($this->conexionApi);
-            }   
+            }
         }
         return $estadoConexion;
-  }
-  
-  public function desconectar(){
-      return curl_close($this->conexionApi);
-  }
-  
-  public function ejecutar($componente,$controlador,$operacion, array $parametros = null){
-      
-    $estadoConexion = false;
-    
-    $this->conexionApi = curl_init();
-    curl_setopt($this->conexionApi, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($this->conexionApi, CURLOPT_USERPWD, self::USERNAME.":".self::PASSWORD);
-    curl_setopt($this->conexionApi, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    curl_setopt($this->conexionApi, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($this->conexionApi, CURLOPT_RETURNTRANSFER, true);
-    $urlCompleta = self::URL.$componente."/".$controlador."/".$operacion;
-    if(!is_null($parametros)){
-        foreach($parametros as $parametro){
-            $urlCompleta .= "/".$parametro;
+    }
+
+    public function desconectar() {
+        return curl_close($this->conexionApi);
+    }
+
+    public function ejecutar($componente, $controlador, $operacion, array $parametros = null) {
+
+        $estadoConexion = false;
+
+        $this->conexionApi = curl_init();
+        curl_setopt($this->conexionApi, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->conexionApi, CURLOPT_USERPWD, self::USERNAME . ":" . self::PASSWORD);
+        curl_setopt($this->conexionApi, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($this->conexionApi, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($this->conexionApi, CURLOPT_RETURNTRANSFER, true);
+        $urlCompleta = self::URL . $componente . "/" . $controlador . "/" . $operacion;
+        if (!is_null($parametros)) {
+            foreach ($parametros as $parametro) {
+                $urlCompleta .= "/" . $parametro;
+            }
         }
+        curl_setopt($this->conexionApi, CURLOPT_URL, $urlCompleta);
+        $resultado = curl_exec($this->conexionApi);
+        //var_dump(json_decode($result, true));
+        $respuesta = json_decode($resultado);
+        if (json_last_error() === JSON_ERROR_NONE) {
+            if ($respuesta->RESPUESTA == 'EXITO') {
+                session_start();
+                $estadoConexion = $_SESSION['API_CONEXION'] = true;
+                session_write_close();
+                $info = curl_getinfo($this->conexionApi);
+            }
+        }
+        $this->desconectar();
+        return $resultado;
     }
-    curl_setopt($this->conexionApi, CURLOPT_URL, $urlCompleta);
-    $resultado =curl_exec($this->conexionApi);
-    //var_dump(json_decode($result, true));
-    $respuesta = json_decode($resultado);
-    if (json_last_error() === JSON_ERROR_NONE) {
-        if($respuesta->RESPUESTA == 'EXITO'){
-            session_start();
-            $estadoConexion = $_SESSION['API_CONEXION'] = true;
-            session_write_close();
-            $info = curl_getinfo($this->conexionApi);
-        }   
-    }
-    $this->desconectar();
-    return $resultado;
-  }
+
 }
 
 $Api = new ApiSicam();
