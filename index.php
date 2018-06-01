7723a7116a44596822310608343fa17ede63bec3
Juan Pablo Llinas Ramirez <coordinacion.innovacion@ccsm.org.co>
2018-06-01 05:49:53 +0000

    nos integramos con el api de speech de google. YA el puse voz a esta nota.

--- index.php
+++ index.php
@@ -32,8 +32,8 @@
   <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
   <script type="text/javascript">
     var _apisicam = _apisicam || {};
-    // _apisicam.apiURL = 'https://api.ccsm.org.co/';
-    _apisicam.apiURL = 'https://sicam32-jpllinas.c9users.io/api/';
+    _apisicam.apiURL = 'https://api.ccsm.org.co/';
+    // _apisicam.apiURL = 'https://sicam32-jpllinas.c9users.io/api/';
     _apisicam.clavePublica = 'm8mcJFLAGT5ba%2FP%2BUNITJv3jf9%2FU4zDM2DERnNFpMjGhC1xXlFAPleRAorZVikJA';
     _apisicam.clavePrivada = 'l6LHlDIvNrsuFtLDZx0ti80%2BZltejZmFVokVWczuEuU%3D';
     window.apisicam || (function(d) {
