aed5219920db6161609729027eceb1ccd19991cc
Juan Pablo Llinás Ramírez <coordinacion.innovacion@ccsm.org.co>
2024-11-28 17:53:46 -0500

    200241128

--- _api.php
+++ _api.php
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:_api.php
 <?php
 require('libs/ApiSicam.clase.php');
 switch ($_POST['operacion']) {
@@ -22,31 +21,3 @@ switch ($_POST['operacion']) {
     default:
         break;
 }
-========
-<?php
-session_start();
-class TurnosControlador {
-
-    public static function siguiente($sedeID) {
-        $ObjTurnos = Turnos::getTurnosLlamandoPorSede($sedeID);
-        echo json_encode(array('success' => 'Se completó la consulta.', 'turnos' => $ObjTurnos));
-    }
-
-    public static function atendiendo($sedeID) {
-        $ObjTurnos = Turnos::getTurnosAtendiendoPorSede($sedeID);
-        echo json_encode(array('success' => 'Se completó la consulta.', 'turnos' => $ObjTurnos));
-    }
-
-}
-
-spl_autoload_register(function ($nombre_clase) {
-    include '/home/ccsm/sicam32/api.sicam/app/modelos/' . $nombre_clase . '.php';
-});
-//include '/home/ccsm/sicam32/api.sicam/app/controladores/Turnos.php';
-$datos = explode("/", $_GET["url"]);
-$nombreContolador = ucfirst($datos[3]) . "Controlador";
-$Controlador = new $nombreContolador();
-$Controlador->$datos[4]($datos[6]);
-
-session_write_close();
->>>>>>>> ed9082a55855cf91464fd4f49d55a49089e917eb:api.php
