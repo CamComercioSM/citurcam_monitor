b2a24fe45a602ee378b61fc1b853b98365cee628
Juan Pablo Llinás Ramírez <coordinacion.innovacion@ccsm.org.co>
2024-11-28 16:10:45 -0500

    copia vieja

--- api.php
+++ api.php
@@ -1,19 +1,26 @@
-<?php
-require('libs/ApiSicam.clase.php');
-switch ($_POST['operacion']) {
-    
-    case 'turnosLlamando':
-        echo $Api->ejecutar( 'atencionpublico', 'TurnosApp', 'mostrarLlamando', array( $_POST['sedeID'], $_POST['areaID'] ) );
-        break;
-    
-    case 'turnosAtendiendo':
-        echo $Api->ejecutar( 'atencionpublico', 'TurnosApp', 'mostrarAtendiendo', array( $_POST['sedeID'], $_POST['areaID']  ) );
-        break;
-        
-    case 'modulosActivos':
-        echo $Api->ejecutar( 'atencionpublico', 'TurnosApp', 'mostrarModulosAtencionActivosPorSede', array( $_POST['sedeID'], $_POST['areaID']  ) );
-        break;
-        
-    default:
-        break;
-}
+<?php
+session_start();
+class TurnosControlador {
+
+    public static function siguiente($sedeID) {
+        $ObjTurnos = Turnos::getTurnosLlamandoPorSede($sedeID);
+        echo json_encode(array('success' => 'Se completó la consulta.', 'turnos' => $ObjTurnos));
+    }
+
+    public static function atendiendo($sedeID) {
+        $ObjTurnos = Turnos::getTurnosAtendiendoPorSede($sedeID);
+        echo json_encode(array('success' => 'Se completó la consulta.', 'turnos' => $ObjTurnos));
+    }
+
+}
+
+spl_autoload_register(function ($nombre_clase) {
+    include '/home/ccsm/sicam32/api.sicam/app/modelos/' . $nombre_clase . '.php';
+});
+//include '/home/ccsm/sicam32/api.sicam/app/controladores/Turnos.php';
+$datos = explode("/", $_GET["url"]);
+$nombreContolador = ucfirst($datos[3]) . "Controlador";
+$Controlador = new $nombreContolador();
+$Controlador->$datos[4]($datos[6]);
+
+session_write_close();
--- index.php
+++ index.php
@@ -1,115 +1,58 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Monitor del sistema de Citas y Turnos de la CamComercioSM | CiTurCam</title>
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  
-  <!-- jQuery library -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-  
-  <!-- Latest compiled JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>    
-  <!--<script src="js/vendor/bootstrap.min.js"></script>-->
-  
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.min.css">
-  <link type="text/css" rel="stylesheet" href="css/fuentes.css">
-  <link type="text/css" rel="stylesheet" href="css/animate.css">
-  <link type="text/css" rel="stylesheet" href="css/rcarousel.css" />
-  <link type="text/css" rel="stylesheet" href="css/main.css">
-  <link type="text/css" rel="stylesheet" href="css/flipclock.css">
-	<link type="text/css" rel="stylesheet" href="js/vendor/AudioPlayer/css/audioplayer.css" />
-  <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-  <script src="js/vendor/owl.carousel/dist/owl.carousel.min.js"></script>
-  <script src="js/vendor/AudioPlayer/js/audioplayer.js"></script>
-
-  
-  
-  <!--//Integración con SICAM-->
-  <script type="text/javascript">
-    var tituloSistema = " CiTurCam - Citas y Turnos de la Camara de Comercio de Santa Marta para el Magdalena" ;                                      
-    var urlSistema = "";
-    var URL_SICAM = "https://si.sicam32.net/";
-    var _apisicam = _apisicam || {};
-    _apisicam.apiURL = 'https://api.sicam32.net/';
-    // _apisicam.apiURL = 'https://sicam32-jpllinas.c9users.io/api/';
-    _apisicam.clavePublica = 'm8mcJFLAGT5ba%2FP%2BUNITJv3jf9%2FU4zDM2DERnNFpMjGhC1xXlFAPleRAorZVikJA';
-    _apisicam.clavePrivada = 'l6LHlDIvNrsuFtLDZx0ti80%2BZltejZmFVokVWczuEuU%3D';    
-    window.apisicam || (function(d) {
-        var s, c, o = apisicam = function() {
-            o._.push(arguments)
-        };
-        o._ = [];
-        s = d.getElementsByTagName('script')[0];
-        c = d.createElement('script');
-        c.type = 'text/javascript'; 
-        c.charset = 'utf-8';
-        c.async = false;
-        c.src = 'https://cliente.api.sicam32.net/javascript/index.php?'+_apisicam.clavePublica+':'+_apisicam.clavePrivada;
-        s.parentNode.insertBefore(c, s);
-    })(document);
-  </script>   
-  <script type="text/javascript" src="https://si.sicam32.net/libs/js/utilidades.js"></script>
-  <script type="text/javascript" src="https://si.sicam32.net/libs/js/pantalla.js"></script>
-  <script type="text/javascript" src="https://si.sicam32.net/libs/js/sonidos.js"></script>
-  <script type="text/javascript" src="https://si.sicam32.net/libs/js/funciones.js"></script>
-</head>
-<body>
-  <div id="pnl-control-vistas" class="flip-container" ontouchstart="this.classList.toggle('hover');">
-  	<div class="flipper">
-  		<div class="front">
-        <div id="areaTrabajo" ></div>
-        <hr>
-        <footer class="text-center">
-          <div class="container">
-            <div class="row">
-              <div class="col-xs-12">
-                <p>Copyright © Cámara de Comercio de Santa Marta para el Magdalena.</p>
-              </div>
-            </div>
-          </div>
-        </footer>    
-      </div>
-  		<div class="back">      
-  		  <div id="areaTrabajo2" ></div>
-      </div>
-  	</div>
-  </div>
-  
-  <div id="cargando"></div>
-  <div id="codigoOculto" style="position: absolute; display: none;" ></div>
-  <div id="audioOculto" style="display:none;" >
-    <!--<audio id="timbreLlamado" >-->
-    <!--  <source src="snd/timbre.mp3" type="audio/mpeg" >-->
-    <!--</audio>-->
-  </div>
-
-  <script type="text/javascript" src="js/sweetalert2.all.js"></script>
-  <script type="text/javascript" src="js/plugins.js"></script>
-  <script type="text/javascript" src="js/vendor/jquery.ui.core.js"></script>
-  <script type="text/javascript" src="js/vendor/jquery.ui.widget.js"></script>
-  <script type="text/javascript" src="js/vendor/jquery.ui.rcarousel.js"></script>
-  <script type="text/javascript" src="js/main.js"></script>
-  <script type="text/javascript" >
-  bloqueoCargando();
-  $(window).on('load',function() {
-      inciarMonitorTurnosCCSM();
-      desbloqueoCargando();
-  });
-  // $(window).on('load',function() {
-  //   $( '#timbreLlamado' ).audioPlayer();
-  // });
-  </script> 
-</body>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Monitor de Turnos - Cámara de Comercio de Santa Marta para el Magdalena</title>
+        <meta name="description" content="Monitor de Turnos">
+        <meta name="author" content="Cámara de Comercio de Santa Marta para el Magdalena">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/flipclock.css">
+        <link href="css/style.css" rel="stylesheet">
+
+
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jsrsasign-latest-all-min.js" type="text/javascript"></script>
+        <script src="js/auth_script.js" type="text/javascript"></script>
+        <script src="js/flipclock.js"></script>
+        <script src="js/scripts.js"></script>
+    </head>
+    <body style="padding-top: 20px;">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4">  
+                    <div id="proximo_turno" >
+                        <?php include('./html/sig-turno.php'); ?>                        
+                    </div>
+                    <hr />
+                    <div id="atendiendo_turno" >
+                        <?php include('./html/atiende-turno.php'); ?>                        
+                    </div>
+                </div>               
+                <div class="col-md-8" style="z-index: -1;">
+                    <img alt="Logo" src="https://www.ccsm.org.co/images/logo314.png"  style="z-index: -1;max-width: 100%;width: 270px;" />
+                    <div id="reloj" class="clock"  style="z-index: -1;margin:0em;float: right;width: auto;"></div>
+                    <hr />
+                    <div class="media"  style="z-index: -1;">                        
+                        <iframe width="100%" height="500"  style="z-index: -1;"
+                                src="https://www.youtube.com/embed/videoseries?list=PLy0Q2cGnTqFs06X1YANLubH8BtWjOwDUj&autoplay=1&loop=1" 
+                                frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script type="text/javascript">
+            var clock;
+            $(document).ready(function () {
+                clock = $('#reloj').FlipClock({
+                    clockFace: 'TwelveHourClock',
+                    language: 'es'
+                });
+            });
+        </script>
+    </body>
 </html>
