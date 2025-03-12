07a519a42b681641ded55ec837f68e888994757f
CesarTorresCCSM <catorres@ccsm.org.co>
2025-03-12 11:20:31 -0500

    ajuste conexionAPI

--- js/wsCITURCAM/api.js
+++ js/wsCITURCAM/api.js
@@ -7,7 +7,8 @@ async function conexionAPI(operacion = '', data = {}) {
                 cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
                 credentials: 'same-origin', // include, *same-origin, omit
                 headers: {
-                        'Content-Type': 'application/json'
+                        'Content-Type': 'application/json',
+                        'origin': 'https://citurcam.com'
                                 // 'Content-Type': 'application/x-www-form-urlencoded',
                 },
                 redirect: 'follow', // manual, *follow, error
