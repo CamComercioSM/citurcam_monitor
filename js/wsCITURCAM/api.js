6988bc994b14c9d9f35edeb8dfa063eb417eb50f
CesarTorresCCSM <catorres@ccsm.org.co>
2025-03-28 16:07:47 -0500

    cambio a apisicam

--- js/wsCITURCAM/api.js
+++ js/wsCITURCAM/api.js
@@ -1,7 +1,8 @@
 // Ejemplo implementando el metodo POST:
 async function conexionAPI(operacion = '', data = {}) {
         // Opciones por defecto estan marcadas con un *
-        const response = await fetch("https://api.citurcam.com/" + operacion, {
+        var datos = {'operacion':operacion, 'zonas': data};
+        const response = await fetch("https://apisicam.net/citurcam/", {
                 method: 'POST', // *GET, POST, PUT, DELETE, etc.
                 mode: 'cors', // no-cors, *cors, same-origin
                 cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -13,7 +14,7 @@ async function conexionAPI(operacion = '', data = {}) {
                 },
                 redirect: 'follow', // manual, *follow, error
                 referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-                body:  JSON.stringify(data) // body data type must match "Content-Type" header
+                body:  JSON.stringify(datos) // body data type must match "Content-Type" header
         });
         return response.json(); // parses JSON response into native JavaScript objects
 }
